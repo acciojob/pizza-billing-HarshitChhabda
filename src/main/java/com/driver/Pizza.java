53b0fa74498bd4107146d17fe43202f9b2be9547
Harshit Chhabda <harshit.chhabda@gmail.com>
2023-02-11 18:50:36 +0530

    Done piza

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,10 +5,33 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
+    private int topping;
+    public boolean isaddChees = false;
+    public boolean isaddtopping = false;
+
+    public boolean isbill = false;
+
+    public boolean istake = false;
+
+
+
+
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
         // your code goes here
+        if(isVeg)
+        {
+            this.price =300;
+            this.topping = 70;
+        }
+            else
+        {
+            this.price=400;
+            this.topping =120;
+
+        }
+            this.bill="Base Price Of The Pizza: "+this.price+"\n";
     }
 
     public int getPrice(){
@@ -16,19 +39,50 @@ public class Pizza {
     }
 
     public void addExtraCheese(){
-        // your code goes here
+       if(!isaddChees)
+       {
+           this.price+=80;
+           isaddChees = true;
+       }
     }
 
     public void addExtraToppings(){
-        // your code goes here
+       if(!isaddtopping)
+       {
+           this.price+=this.topping;
+           isaddtopping=true;
+       }
     }
 
     public void addTakeaway(){
-        // your code goes here
+     if(!istake)
+     {
+         this.price+=20;
+         istake=true;
+     }
     }
 
     public String getBill(){
-        // your code goes here
-        return this.bill;
+        if(!isbill)
+        {
+            if(isaddChees)
+            {
+                this.bill+="Extra Cheese Added: 80"+"\n";
+            }
+            if(isaddtopping)
+            {
+                this.bill+="Extra Toppings Added: "+this.topping+"\n";
+            }
+            if(istake)
+            {
+                this.bill+="Paperbag Added: 20";
+            }
+            isbill = true;
+            return this.bill+="Total Price: "+this.price;
+
+        }
+        return "";
+
+
     }
 }
